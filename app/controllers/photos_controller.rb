b6bbff8da56d77e7a516f21006c84785b93f837c
Brooke Perry and katrina hockman <katrina@>
2018-12-12 13:59:14 -0800

    root successfully to photos index

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,22 @@
 class PhotosController < ApplicationController
-  def index
-    @photos = photo.all
-  end
+    def new
+      @user = List.find(params[:user_id])
+      @photo = @user.photos.new
+      # render :photos
+    end
 
-  
-end
+    def create
+      @user = List.find(params[:user_id])
+      @photo = @user.photos.new(photo_params)
+      if @photo.save
+        redirect_to user_path(@photo.user)
+      else
+        render :new
+      end
+    end
+
+    private
+      def photo_params
+        params.require(:photo).permit(:date_seen)
+      end
+  end
